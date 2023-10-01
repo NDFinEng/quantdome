86417a682603793be0b2cf5aa9fa5c8b98bc17b3
rckenn11 <rckennedy1@gmail.com>
2023-10-01 17:17:23 +0100

    Add connection check

--- quantdome/kafka-data.py
+++ quantdome/kafka-data.py
@@ -7,7 +7,12 @@ import pandas as pd
 
 class kafka_producer():
     def __init__(self):
-        self.producer = kp(client_id='Data Handler')
+        server_id = 'localhost:9092'
+        self.producer = kp(bootstrap_servers=server_id, client_id='Data Handler')
+
+        if not self.producer.bootstrap_connected():
+            raise Exception(f'Cannot connect to {server_id}. Please check connection and try again.')
+
 
 ''' Not sure if it is necessary to separate the two:
 class backtest_kafka_producer():
