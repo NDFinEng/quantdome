417312234134b45e8d1dbb3854167b6524b91830
rckenn11 <rckennedy1@gmail.com>
2023-10-15 20:17:34 +0100

    Inital producer code (untested)

--- quantdome/kafka-data.py
+++ quantdome/kafka-data.py
@@ -3,17 +3,23 @@ from . import data
 
 # Third Party Imports
 from kafka import KafkaProducer as kp
-import pandas as pd
+from json import dumps
 
 class kafka_producer():
     def __init__(self):
-        server_id = 'localhost:9092'
-        self.producer = kp(bootstrap_servers=server_id, client_id='Data Handler')
+        server_id = 'quantdome.crc.nd.edu:8092'
+        self.producer = kp(
+            bootstrap_servers=server_id, 
+            client_id='Data Handler',
+            value_serializer=lambda x: dumps(x).encode('utf-8')
+        )
 
         if not self.producer.bootstrap_connected():
             raise Exception(f'Cannot connect to {server_id}. Please check connection and try again.')
+        
+    def send(self, type, name, value):
+        self.producer.send(type, value=value, partition=name)
 
-
-''' Not sure if it is necessary to separate the two:
-class backtest_kafka_producer():
-    pass'''
+    def names(self, type):
+        for name in self.producer.partitions_for(type):
+            print(name)
